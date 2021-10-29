13d6a1fa9e30ccbcb812172bc931d74cad6977c9
Patricio Fajardo <apatricio.fajardoc@gmail.com>
2021-10-29 11:21:29 -0500

    add actualizar y eliminar proyecto

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -36,4 +36,78 @@ const obtenerProyectos = async (req, res) => {
   }
 };
 
-export { crearProyecto, obtenerProyectos };
+// actualizar un proyecto
+const actualizarProyecto = async (req, res) => {
+  // revisar si hay errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+
+  // extraer la información
+
+  const { nombre } = req.body;
+  const nuevoProyecto = {};
+  if (nombre) {
+    nuevoProyecto.nombre = nombre;
+  }
+
+  try {
+    //   revisar el ID
+    let proyecto = await Proyecto.findById(req.params.id);
+
+    // si el proyecto existe o no
+    if (!proyecto) {
+      return res
+        .status(404)
+        .json({ msg: "Proyecto a actualizar no encontrado" });
+    }
+
+    // verificar el creado del proyecto
+    if (proyecto.creador.toString() !== req.usuario.id) {
+      res.status(401).json({ msg: "Usuario no autorizado" });
+    }
+
+    // actualizar el proyecto
+    proyecto = await Proyecto.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $set: nuevoProyecto },
+      { new: true }
+    );
+    res.send({ proyecto });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error al actualizar el proyecto");
+  }
+};
+
+// eliminar un proyecto por su ID
+const eliminarProyecto = async (req, res) => {
+  try {
+    //   revisar el ID
+    let proyecto = await Proyecto.findById(req.params.id);
+
+    // si el proyecto existe o no
+    if (!proyecto) {
+      return res.status(404).json({ msg: "Proyecto a eliminar no encontrado" });
+    }
+
+    // verificar el creado del proyecto
+    if (proyecto.creador.toString() !== req.usuario.id) {
+      res.status(401).json({ msg: "Usuario no autorizado" });
+    }
+
+    // eliminar el proyecto
+    proyecto = await Proyecto.findOneAndRemove({ _id: req.params.id });
+    res.json({ msg: "Proyecto Eliminado" });
+  } catch (error) {
+    console.log(error);
+    res.status(404).send("Hubo un error al momento de eliminar un proyecto");
+  }
+};
+export {
+  crearProyecto,
+  obtenerProyectos,
+  actualizarProyecto,
+  eliminarProyecto,
+};
--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { check } from "express-validator";
 import {
+  actualizarProyecto,
   crearProyecto,
+  eliminarProyecto,
   obtenerProyectos,
 } from "../controllers/proyectoController.js";
 import authMiddleware from "../moddelware/authMiddleware.js";
@@ -16,6 +18,19 @@ router.post(
   [check("nombre", "El nombre del proyecto es obligatorio").not().isEmpty()],
   crearProyecto
 );
+
+// obtener todos los proyectos de un usuario
 router.get("/", authMiddleware, obtenerProyectos);
+
+// actualizar el proyecto via ID
+router.put(
+  "/:id",
+  authMiddleware,
+  [check("nombre", "El nombre del proyecto es obligatorio").not().isEmpty()],
+  actualizarProyecto
+);
+
+// Eliminar Proyecto
+router.delete("/:id", authMiddleware, eliminarProyecto);
 
 export default router;
