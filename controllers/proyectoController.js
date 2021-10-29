ab629adaf192b17954a7e68079e96e43915c6380
Patricio Fajardo <apatricio.fajardoc@gmail.com>
2021-10-29 10:45:08 -0500

    Obtener proyectos de un usuario en JSON

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,6 +1,13 @@
+import { validationResult } from "express-validator";
 import Proyecto from "../models/Proyecto.js";
 
 const crearProyecto = async (req, res) => {
+  // revisar si hay errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+
   try {
     //   crear un nuevo    proyecto
     const proyecto = new Proyecto(req.body);
@@ -15,4 +22,18 @@ const crearProyecto = async (req, res) => {
   }
 };
 
-export default crearProyecto;
+// obtienes todos los proyectos del usuario actual
+const obtenerProyectos = async (req, res) => {
+  try {
+    //   obtener los proyectos
+    const proyectos = await Proyecto.find({ creador: req.usuario.id }).sort({
+      creado: -1,
+    });
+    res.send(proyectos);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error al obtener los proyectos del usuario");
+  }
+};
+
+export { crearProyecto, obtenerProyectos };
--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,12 +1,21 @@
 import express from "express";
-import crearProyecto from "../controllers/proyectoController.js";
+import { check } from "express-validator";
+import {
+  crearProyecto,
+  obtenerProyectos,
+} from "../controllers/proyectoController.js";
 import authMiddleware from "../moddelware/authMiddleware.js";
 
 const router = express.Router();
 
 // crear proyectos
 // api/proyectos
-router.post("/", authMiddleware, crearProyecto);
-router.get("/", authMiddleware, crearProyecto);
+router.post(
+  "/",
+  authMiddleware,
+  [check("nombre", "El nombre del proyecto es obligatorio").not().isEmpty()],
+  crearProyecto
+);
+router.get("/", authMiddleware, obtenerProyectos);
 
 export default router;
